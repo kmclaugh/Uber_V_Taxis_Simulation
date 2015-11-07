48414322192206ba6a9c9401f91496d87e430df2
Kevin McLaughlin <kevin.mclaughlin70@gmail.com>
2015-11-07 10:32:54 -0600

    Finished the passenger class and how the cars pick up the passenger.

--- uber_v_taxis.js
+++ uber_v_taxis.js
@@ -1,15 +1,17 @@
-var total_count = 0;
-var straight_count = 0;
-var left_count = 0;
-var right_count = 0;
 var timer = null;
-var real_time_per_step = 10;
+var real_time_per_step = 500;
+var cars_list = [];
 var total_steps = 0;
+var total_rides_started = 0;
+var total_rides_completed = 0;
+var total_wait_time = 0;
+var total_ride_time = 0;
+var the_grid;
 
 $(window).load(function () {
     
     $(document).ready(function () {
-        var the_grid = new grid_class(18);
+        the_grid = new grid_class(18);
         the_grid.create_grid();
         the_grid.add_headings();
         the_grid.add_options();
@@ -17,8 +19,17 @@ $(window).load(function () {
         
         $('#grid').append(the_grid.html);
         
-        car = new car_class(9,4, 'west', the_grid);
-        car.set_on(9,4);
+        car = new car_class('north', the_grid);
+        car.set_on(13,12);
+        cars_list.push(car);
+        car = new car_class('north', the_grid);
+        car.set_on(9,2);
+        cars_list.push(car);
+        
+        passenger = new passenger_class(the_grid);
+        passenger.set_on(13,10);
+        passenger = new passenger_class(the_grid);
+        passenger.set_on(13,11);
         
         $("#start").click(function() {
             if (timer !== null) return;
@@ -54,26 +65,74 @@ $(window).load(function () {
 });
 
 function time_step(){
-    car.move();
+    for (c=0; c<cars_list.length; c++){
+        car = cars_list[c];
+        car.move();
+    }
     total_steps += 1;
+    console.log(the_grid.get_cell(13, 10).passengers);
     $('#total_steps').text(total_steps);
 }
 
-function update_counts(){
-     $('#straight_count').text(straight_count/total_count);
-     $('#right_count').text(right_count/total_count);
-     $('#left_count').text(left_count/total_count);
+function passenger_class(grid){
+    /*Class for the passenger*/
+    this.grid = grid;
+    this.current_cell = false;
+    this.waiting = true;
+    this.destination_travel_time = 5;//getRandomInt(min=4, max=100);
+    this.start_step = total_steps;
+    this.wait_time;
+    
+    this.current_travel_time = 0;
+    
+    this.picked_up = function(){
+        /*Logic for being picked up by the car*/
+        this.remove_from();
+        this.waiting = false;
+        this.wait_time = total_steps+1 - this.start_step;
+        total_rides_started += 1;
+        total_wait_time += this.wait_time;
+        var average_wait_time = total_wait_time/total_rides_started;
+        $('#average_wait_time').text(average_wait_time);
+    }
+        
+    this.dropped_off = function(){
+        /*logic for being dropped off*/
+        total_ride_time += this.destination_travel_time;
+        total_rides_completed += 1;
+        var average_ride_time = total_ride_time/total_rides_completed;
+        $('#average_ride_time').text(average_ride_time);
+    }
+    
+    this.set_on = function(x,y){
+        /*Places the passenger on the given x, y cell of the grid*/
+        var new_cell = this.grid.get_cell(x, y);
+        if (new_cell != false && new_cell.obstacle == false){
+            $('#'+new_cell.html_id).addClass('has_passenger');
+            this.current_cell = new_cell;
+            this.current_cell.passengers.push(this);
+        }
+        return new_cell;
+    }
+    this.remove_from = function(){
+        /*removes the passenger from it's current cell*/
+        if (this.current_cell != false){
+            this.current_cell.passengers.shift();
+            this.current_cell = false;
+        }
+    }
 }
 
-function car_class(start_x, start_y, start_heading, grid){
+function car_class(start_heading, grid){
     /*class for the car*/
-    this.x = start_x;
-    this.y = start_y;
+    this.x;
+    this.y;
     this.heading = start_heading;
     this.grid = grid;
     this.current_cell = false;
     this.next_move = false;
     this.next_next_move = false;
+    this.passenger = false;
     
     this.move = function(){
         if (this.next_move != false){
@@ -87,50 +146,77 @@ function car_class(start_x, start_y, start_heading, grid){
         else{
             this.pick_move();
         }
+        this.passenger_logic();
     }
+    
+    this.passenger_logic = function(){
+        /*logic for picking up, or dropping off a passenger*/
+        if (this.passenger == false){
+            if (this.current_cell.passengers.length > 0 ){
+                this.passenger = this.current_cell.passengers[0];
+                this.passenger.picked_up();
+            }
+        }
+        else{
+            this.passenger.current_travel_time += 1;
+            if (this.passenger.current_travel_time == this.passenger.destination_travel_time){
+                this.passenger.dropped_off();
+                if (this.current_cell.passengers.length == 0){
+                    $('#'+this.current_cell.html_id).removeClass('has_passenger');
+                }
+                this.passenger = false;
+            }
+        }
+    }
+            
     
     this.pick_move = function(){
         /*select which direction the car will move based on the available options
          *of the current cell*/
-        total_count += 1;
-        $('#total_count').text(total_count);
         var valid_options = this.current_cell.valid_options[this.heading];
         var selected_move = valid_options[Math.floor(valid_options.length * Math.random())];
-        console.log(selected_move);
         if (selected_move == 'straight'){
-            this.go_straight();
-            straight_count += 1;
-            update_counts();
+            if (valid_options.length > 1){
+                this.go_straight(intersection=true);
+            }
+            else{
+                this.go_straight(intersection=false);
+            }
+            
         }
         if (selected_move == 'right'){
             this.turn_right();
-            right_count += 1;
-            update_counts();
         }
         if (selected_move == 'left'){
             this.turn_left();
-            left_count += 1;
-            update_counts();
         }
     }
     
-    this.go_straight = function(){
+    this.go_straight = function(intersection){
         /*steps to go through an intercection based on current position and heading*/
         if (this.heading == 'east'){
             this.move_east();
-            this.next_move = this.move_east;
+            if (intersection == true){
+                this.next_move = this.move_east;
+            }
         }
         else if (this.heading == 'south'){
             this.move_south();
-            this.next_move = this.move_south;
+            if (intersection == true){
+                this.next_move = this.move_south;
+            }
         }
         else if (this.heading == 'north'){
             this.move_north();
-            this.next_move = this.move_north;
+            if (intersection == true){
+                this.next_move = this.move_north;
+            }
         }
         else if (this.heading == 'west'){
             this.move_west();
-            this.next_move = this.move_west;
+            if (intersection == true){
+                this.next_move = this.move_west;
+            }
         }
     }
     
@@ -201,6 +287,9 @@ function car_class(start_x, start_y, start_heading, grid){
         if (new_cell != false && new_cell.obstacle == false){
             this.remove_from();
             $('#'+new_cell.html_id).addClass('has_car');
+            if (this.passenger != false){
+                $('#'+new_cell.html_id).addClass('has_passenger');
+            }
             this.current_cell = new_cell;
         }
         return new_cell;
@@ -209,6 +298,9 @@ function car_class(start_x, start_y, start_heading, grid){
         /*removes the car from it's current cell*/
         if (this.current_cell != false){
             $('#'+this.current_cell.html_id).removeClass('has_car');
+            if (this.passenger != false && this.current_cell.passengers.length == 0 ){
+                $('#'+this.current_cell.html_id).removeClass('has_passenger');
+            }
         }
     }
 }
@@ -388,6 +480,7 @@ function cell_class(x, y, obstacle){
     this.valid_options = {};
     this.html = '';
     this.html_id = this.x+"-"+this.y;
+    this.passengers = [];
     
     this.make_html = function(){
         if (this.obstacle == false){
@@ -425,4 +518,9 @@ function headings(){
         }
         return headings_string;
     }
+}
+
+function getRandomInt(min, max) {
+    /*Returns a random integer between min (inclusive) and max (inclusive)*/
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
